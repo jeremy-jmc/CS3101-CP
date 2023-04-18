4c8abec41a01c034e23758b88d21000811caf474
JerM1609 <jeremy.matos@utec.edu.pe>
2023-04-18 01:45:21 -0500

    S3

--- cf.cpp
+++ cf.cpp
@@ -1,5 +1,4 @@
-// #pragma GCC optimize("Ofast")
-// #pragma GCC target("sse4")
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,28 +6,39 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, int> plli;
 
-#define speed ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0)
+#define speed                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0), cout.tie(0)
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
 #define sz(x) ((int)(x).size())
 #define endll '\n'
-#define yes cout<<"YES"<<endll
-#define no cout<<"NO"<<endll
+#define yes cout << "YES" << endll
+#define no cout << "NO" << endll
 #define pb push_back
-// #define f(i,a,b) for( ll i = a; i < b ; i++ ) 
+// #define f(i,a,b) for( ll i = a; i < b ; i++ )
 // #define fr(i,a,b) for(ll i = a; i >= b; i--)
 
 const int MX = 1e5 + 10;
-const ll mod = 1e9+7;      // 998244353
-const ll INF = 1LL<<20;
+const ll mod = 1e9 + 7; // 998244353
+const ll INF = 1LL << 20;
 
-template<typename T, typename V>
-ostream& operator << (ostream& os, pair<T, V> pr)
-{ os << "{" << pr.first << ", " << pr.second << "}";  return os; }
-
-template<template<class, class> class Container, typename T>
-ostream& operator<<(ostream& os, const Container<T, allocator<T>>& container)
-{ os<<"[ "; for(auto el : container) os << el << " "; os<<"]"; return os; }
+template <typename T, typename V>
+ostream &operator<<(ostream &os, pair<T, V> pr)
+{
+    os << "{" << pr.first << ", " << pr.second << "}";
+    return os;
+}
+xor
+template <template <class, class> class Container, typename T>
+ostream &operator<<(ostream &os, const Container<T, allocator<T>> &container)
+{
+    os << "[ ";
+    for (auto el : container)
+        os << el << " ";
+    os << "]";
+    return os;
+}
 
 // template<
 //     template<class, class> class C1,
@@ -61,46 +71,47 @@ ostream& operator<<(ostream& os, const Container<T, allocator<T>>& container)
 //     return true;
 // }
 
-// bool mod_(ll num) { 
+// bool mod_(ll num) {
 //     if (num > mod)
 //         return num - mod;
 //     return num;
 // }
 
+// ll binpow(ll a, int b) {
+//     // return pow(a, b);
+//     ll res = 1;
+//     while (b > 0) {
+//         if (b & 1)
+//             res = res * a;
+//         a = a * a;
+//         b >>= 1;
+//     }
+//     return res;
+// }
+
 void solve()
 {
-    ll N;
-    cin >> N;
-    ll ans = 1, denom;
-    for (int i = 2; i <= N + 1; i++)
-    {
-        denom = ((i-1)/2 == 0) ? 1 : (i-1)/2;
-        ans *= (denom+1);
-        ans /= (denom);
-    }
-    cout << ans - 1 << endll;
 }
 
-int main() 
+int main()
 {
-// #ifndef TEST
-//     freopen("input.in", "r", stdin);
-//     freopen("output.out", "w", stdout);
-// #endif
-    // time_t start, end;   
+#ifndef TEST
+    freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+#endif
+    // time_t start, end;
     // time(&start);
     speed;
     ll T = 1;
-    cin>>T;
-    
+    // cin >> T;
     while (T--)
         solve();
-    
+
     // time(&end);
     // double time_taken = double(end - start);
     // cout << "\nTime: " << fixed << time_taken << setprecision(5) << " sec " << endl;
-// #ifdef LOCAL_TIME
-//     cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s\n";
-// #endif
+    // #ifdef LOCAL_TIME
+    //     cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s\n";
+    // #endif
     return 0;
 }
--- code/uva750.cpp
+++ code/uva750.cpp
@@ -2,57 +2,80 @@
 /// 8 queen chess problem
 #include <bits/stdc++.h>
 using namespace std;
-int a,b,canti;
+int a, b, canti;
 int row[8];
 
-void imprimir(){
-    if(canti>0) printf("\n");
+void imprimir()
+{
+    if (canti > 0)
+        printf("\n");
     canti++;
-    printf("%2d      %d", canti, row[0]+1);
-    for(int i=  1;i<8; i++)printf(" %d",row[i]+1);
+    printf("%2d      %d", canti, row[0] + 1);
+    for (int i = 1; i < 8; i++)
+        printf(" %d", row[i] + 1);
 }
 
-bool isvalid(int fila, int nq){
-    for(int i=0;i<nq;i++){
-        if(row[i]==fila || abs(row[i]-fila)==abs(i-nq)) return false;
+bool isvalid(int fila, int nq)
+{   // check if the queen can be in the row 'fila' for the column 'nq'
+    // for each column
+    for (int i = 0; i < nq; i++)
+    {
+        if (row[i] == fila ||       // check it the row of the queen in the column 'i' is the same as the proposal
+            abs(row[i] - fila) == abs(i - nq)       // check if the difference of the rows is the same as the difference of the columns
+            )
+            return false;
     }
     return true;
 }
 
-void backtrack(int nq){
-    if(nq==8 && row[b]==a) imprimir();
-    for(int i=0;i<8;i++){
-        if(isvalid(i,nq)) {
-            row[nq]= i;
-            backtrack(nq+1);
+void backtrack(int nq)
+{   
+    // explore all the solutions
+    if (nq == 8 && row[b] == a)
+        imprimir();
+    
+    // for each row
+    for (int i = 0; i < 8; i++)
+    {   // try setting the row of the column 'nq'
+        if (isvalid(i, nq))
+        {
+            row[nq] = i;            // put the row of the queen in the column 'nq'
+            backtrack(nq + 1);      // explore the next column
         }
     }
 }
 
-void solve(){
-    scanf("%d%d",&a,&b);
+void solve()
+{
+    scanf("%d%d", &a, &b);
     a--;
     b--;
-    canti=0;
+    canti = 0;
     printf("SOLN       COLUMN\n");
     printf(" #      1 2 3 4 5 6 7 8\n\n");
     backtrack(0);
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     scanf("%d", &t);
-    while(t--){
+    while (t--)
+    {
         /// reset values of diags
-        for(int i=0;i<8;i++) row[i]=0;
+        for (int i = 0; i < 8; i++)
+            row[i] = 0;
+            // the row of the queen in the column i
         /// solve
 
         solve();
-        if(t>0) printf("\n\n");
-        else printf("\n");
+        if (t > 0)
+            printf("\n\n");
+        else
+            printf("\n");
     }
     return 0;
 }
